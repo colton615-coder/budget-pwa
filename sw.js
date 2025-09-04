87298465506d27fb2780947cc3b3341edcd91e36
colton615-coder <colton615@gmail.com>
2025-09-03 23:32:14 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Minimal service worker for cache (optional)
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open('bb-cache-v1').then(cache => cache.addAll(['./','./index.html','./app.css','./app.js']).catch(()=>null))
-  );
-});
-self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (event) => { event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))); });
